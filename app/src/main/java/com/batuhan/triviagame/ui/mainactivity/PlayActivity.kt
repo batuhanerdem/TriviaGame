a1885f6b909d7390ce31b2a4e173c3242dc6d530
batuhanerdem <batuhanerdem173@gmail.com>
2022-08-05 16:50:45 +0300

    test

---
+++ app/src/main/java/com/batuhan/triviagame/ui/mainactivity/PlayActivity.kt
@@ -0,0 +1,11 @@
+package com.batuhan.triviagame.ui.mainactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+    }
+}
